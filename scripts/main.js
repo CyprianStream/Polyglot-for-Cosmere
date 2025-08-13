7c870b6716dc27091866e045fa1e6f0fe6982925
cyprian <cyprianstream@gmail.com>
2025-08-12 22:41:44 -0400

    Fixed language grabbing     Languages now are grabbed from tokens as expected where before they would error out or only grab the first language Updated module.json     Added requirements of Cosmere system and Polyglot module     Using esModules instead of scripts. Not really sure why but it seems to work either way     reorganized properties of module.json

--- scripts/main.js
+++ scripts/main.js
@@ -53,19 +53,26 @@ Hooks.once("polyglot.init", (LanguageProvider) => {
             }
         }
 
+        
+
         getUserLanguages(actor) {
             let known_languages = new Set();
             let literate_languages = new Set();
-            console.log("POLYGLOT Grabbing user languages...")
+            console.log("POLYGLOT Grabbing user languages for character:" + actor.name)
+            //console.log(actor.name)
             for (let lang of actor.system.expertises) {
-                console.log(lang)
-                if (this.languages[lang.label.toString()].label === lang.label.toString()){
+                //console.log(lang)
+                //console.log(lang.label)
+                //console.log(this.languages[lang.label].label)
+                if (this.languages[lang.label].label === lang.label){
+                    //console.log("Adding language " + lang.label)
                     known_languages.add(lang.label)
                     literate_languages.add(lang.label)
-                    console.log(lang.label)
                 };
-                return [known_languages, literate_languages];
             }
+            console.log(known_languages)
+            //console.log(literate_languages)
+            return [known_languages, literate_languages];
         }
     }
     game.polyglot.api.registerModule("cosmere-polyglot", CosmereLanguageProvider);
